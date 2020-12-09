3c2d35b32a2b1a397f938aca6249c3be845551ae
Sander van Dragt <sander@vandragt.com>
2020-12-09 16:29:48 +0000

    Fixes #5 Fixes #6. Watch folder for changes in contents hash.

--- ia2web.php
+++ ia2web.php
@@ -4,15 +4,34 @@ require_once __DIR__. '/vendor/autoload.php';
 
 main();
 
-function main() {
+function main() : void {
 	$args = get_args();
 
-	handle_theme($args);
-	$args['files'] = handle_files($args);
-	handle_index($args);
+	watch($args, [
+		'template/*.css' => 'handle_theme',
+		'../*.md' => 'handle_file',
+
+	]);
 }
 
-function get_args() {
+function watch(array $args, $paths) : void {
+	while(true) {
+
+	foreach ($paths as $path => $callback) {
+		// Watch __FILE__ for metadata changes (e.g. mtime)
+		foreach (glob($path) as $source) {
+			$callback($args, $source);
+		}
+	}
+			$abort = sleep(1);
+		if ($abort) {
+			break;
+		}
+	}
+
+}
+
+function get_args() : array {
 	return [
 		'Parsedown' => new Parsedown(),
 		'layout'    => file_get_contents('template/_layout.html'),
@@ -20,26 +39,29 @@ function get_args() {
 	];
 }
 
-function handle_files($args) {
-	$files = [];
-	foreach (glob("../*.md") as $source) {
-		$subject = file_get_contents($source);
-		$content = $args['Parsedown']->text($subject); 
-		$_layout = $args['layout'];
-		$data    = str_replace('{content}', $content, $_layout);
+function handle_file(&$args, $source) {
+	$dest = str_replace ( '.md', '.html', __DIR__ . '/html/' . $args['dir'] . '/' . $source);
 
-		$dest     = __DIR__ . '/html/' . $args['dir'] . '/' . $source;
-		$filename = str_replace ( '.md', '.html', $dest);
-	   	file_put_contents ( $filename, $data );
-
-		$files[] = $source;
-
-		echo "+ ${filename}"  . PHP_EOL;
+	if (isset($args['files'][$source]) && md5_file($source) === $args['files'][$source]) {
+		return;
 	}
-	return $files;
+
+	$subject = file_get_contents($source);
+	$content = $args['Parsedown']->text($subject); 
+	$_layout = $args['layout'];
+	$data    = str_replace('{content}', $content, $_layout);
+
+   	file_put_contents ( $dest, $data );
+
+	$args['files'][$source] = $md5 = md5($subject);
+
+	handle_index($args);
+
+	$md5 = substr($md5, 0, 8);
+	echo "${md5} ${dest}"  . PHP_EOL;
 }
 
-function handle_index($args) {
+function handle_index(array $args) : void {	
 	$content = '<ul>';
 	foreach ($args['files'] as $created => $source) {
 		$link = str_replace ( '.md', '.html', $source);
@@ -55,10 +77,23 @@ function handle_index($args) {
 	$filename = __DIR__ .'/'. $args['dir'] . '/index.html';
    	file_put_contents ( $filename, $data );
 
-	echo "${filename}"  . PHP_EOL;
+   	$md5 = md5($data);
+	$md5 = substr($md5, 0, 8);
+	echo "${md5} ${filename}"  . PHP_EOL;
 }
 
-function handle_theme($args) {
+function handle_theme(array &$args, $source) : void {
 	@mkdir($args['dir']);
-	copy('template/style.css', 'html/style.css');
+	$dest = str_replace('template', $args['dir'], $source);
+
+	$data = file_get_contents($source);
+
+	if (isset($args['files'][$source]) && md5_file($source) === $args['files'][$source]) {
+		return;
+	}
+	copy($source, $dest);
+	$args['files'][$source] = $md5 = md5($data);
+
+	$md5 = substr($md5, 0, 8);
+	echo "${md5} ${dest}"  . PHP_EOL;
 }
