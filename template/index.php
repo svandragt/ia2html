43fc97502cbf0a769dfc60dcfbabd88f273c33dc
Sander van Dragt <sander@vandragt.com>
2021-07-27 23:15:47 +0100

    template fixes

--- template/index.php
+++ template/index.php
@@ -1,12 +1,14 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Yo</title>
+	<title>IA2Web</title>
 	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
+	<nav>IA2Web</nav>
 	<main>
 		<?php echo $this->content; ?>
 	</main>
+	<footer><p>Powered by <a href="https://github.com/svandragt/ia2web">IA2web</a></p></footer>
 </body>
 </html>
--- template/single.php
+++ template/single.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Yo</title>
+	<title>IA2Web</title>
 	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
@@ -9,5 +9,6 @@
 	<main>
 		<?php echo $this->content; ?>
 	</main>
+	<footer><p>Powered by <a href="https://github.com/svandragt/ia2web">IA2web</a></p></footer>
 </body>
 </html>
